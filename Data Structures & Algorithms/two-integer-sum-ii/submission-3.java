2b86d4b3f2abdbe377b485173d21d747471214fa
Sparsh Sharma <53931981+sparshsharma2510@users.noreply.github.com>
2026-07-18 14:25:03 +0800

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] twoSumBetter(int[] numbers, int target){
+        int n = numbers.length;
+        for(int i = 0; i < n-1; i++){
+            int start = i+1;
+            int end = n-1;
+
+            while (start <= end){
+                int mid = (start+end)/2;
+                int currSum = numbers[mid]+numbers[i];
+                if(currSum == target)
+                    return new int[]{i+1, mid+1};
+                else if(currSum < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSumBetterTime(int[] numbers, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+    int n = numbers.length;
+        for(int i = 0 ; i < n; i++)
+            map.put(numbers[i], i);
+        
+        for(int i = 0; i < n; i++){
+            int required = target-numbers[i];
+            if(map.containsKey(required) && map.get(required) != i)
+                return new int[]{i+1, map.get(required)+1};
+        }
+        return new int[]{};
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        return twoSumBetterTime(numbers, target);
+    }
+}
